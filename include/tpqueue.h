489058c56a6d1c3fa988bfabfa25bf56055aa2f3
Axenov Roman <98100948+raxenov0@users.noreply.github.com>
2022-04-24 22:15:50 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -3,76 +3,65 @@
 #define INCLUDE_TPQUEUE_H_
 #include <iostream>
 
-template<typename T>
+template <typename T>
 class TPQueue {
- private:
+private:
     struct ITEM {
-      T value;
-      ITEM* next;
-      ITEM* last;
+        T value;
+        ITEM* next;
+        ITEM* last;
     };
-  ITEM* first, *last;
+    ITEM* last;
+    ITEM* first;
 
- public:
-  TPQueue() {
-    first = nullptr;
-    last = nullptr;
-  }
-  ~TPQueue() {
-     while (first) {
-      ITEM* current = first;
-      first = first->next;
-      delete current;
-      }
-  }
-  bool isEmpty() {
-  if (first == nullptr) return true;
-  else
-    return false;
-  }
-  void push(T element) {
-    ITEM* elem = new ITEM;
-    elem->value = element;
-    elem->next = nullptr;
-    if (isEmpty()) {
-      first = last = elem;
-      first->next = nullptr;
-      first->last = nullptr;
-    } else {
-      ITEM* current = first;
-      while (current != nullptr) {
-        if (current->value.prior < elem->value.prior) {
-          if (current == first) {
-            elem->last = nullptr;
-            first = elem;
-          }
-          if (current->last != nullptr) {
-            elem->next = current;
-            current = current->last;
-            current->next = elem;
-            elem->last = current;
-            current = current->next->next;
-          }
-          elem->next = current;
-          current->last = elem;
-          return;
-        }
-        current = current->next;
-      }
-      last->next = elem;
-      elem->next = nullptr;
-      elem->last = last;
-      last = elem;
+public:
+    TPQueue() {
+        first = last = nullptr;
     }
-    return;
-  }
-  T pop() {
-    ITEM* current = first;
-    first = first->next;
-    T val = current->value;
-    delete current;
-    return val;
-  }
+
+    void push(T element) {
+        ITEM* item_ = new ITEM;
+        item_->value = element;
+        item_->next = item_->last = nullptr;
+        ITEM* current = first;
+        while (current && current->value.prior >= item_->value.prior) current = current->next;
+        
+       if (current == nullptr && first == nullptr) {
+            first = last = item_;
+        }
+        else if (current == nullptr && first) {
+            last->next = item_;
+            item_->last = last;
+            last = item_;
+        } else if (current->last == nullptr) {
+            first->last = item_;
+            item_->next = first;
+            first = item_;
+        } else {
+            current->last->next = item_;
+            item_->last = current->last;
+            item_->next = current;
+            current->last = item_;
+        }
+    }
+    T pop() {
+        ITEM* current = first;
+        if (first->next == nullptr) first = nullptr;
+        else {
+            first = first->next;
+            first->last = nullptr;
+        }
+        T value_ = current->value;
+        delete current;
+        return value_;
+    }
+    void print() {
+        ITEM* current = first;
+        while (current != nullptr) {
+            std::cout << current->value.ch << std::endl;
+            current = current->next;
+        }
+    }
 };
 
 struct SYM {
